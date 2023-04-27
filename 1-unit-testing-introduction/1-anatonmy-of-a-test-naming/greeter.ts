c5c86f7beb6b5a5cabcd376cfb734186a29a6bf2
JuanEliasJabib02 <juaneliasjabib02@gmail.com>
2023-04-27 13:28:15 -0500

    anotomy of a test-naming

---
+++ 1-unit-testing-introduction/1-anatonmy-of-a-test-naming/greeter.ts
@@ -0,0 +1,9 @@
+/* The test and the production name has to be the same */
+
+export function greeter() {
+  return {
+    helloWorld: function () {
+      return "hello world"
+    }
+  }
+}
---
+++ 1-unit-testing-introduction/first.test.ts
@@ -0,0 +1,9 @@
+describe("first test", () => {
+  it("1 shpuld be 1", () => {
+    expect(2).toBe(2);
+  })
+
+  it("2 should  be 2", () => {
+    expect(2).toBe(2);
+  })
+})
