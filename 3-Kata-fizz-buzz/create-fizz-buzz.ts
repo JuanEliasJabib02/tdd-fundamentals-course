4c42ab16a3a8ee9c0057d04e0bd247ef45d8744c
JuanEliasJabib02 <juaneliasjabib02@gmail.com>
2023-05-08 10:16:53 -0500

    kata exercise finish

--- 3-Kata-fizz-buzz/create-fizz-buzz.ts
+++ 3-Kata-fizz-buzz/create-fizz-buzz.ts
@@ -1,13 +1,21 @@
 
 interface FizzBuzz {
-  start(): string
+  start(number: number): string
 }
 
 
 export function createFizzBuzz(): FizzBuzz {
 
+
   return {
 
-  }
+    start(number): string {
+      if (number % 3 === 0 && number % 5 === 0) return "fizz-buzz"
+      else if (number % 3 === 0) return "fizz"
+      else if (number % 5 === 0) return "buzz"
+      else return `${number}`
 
+    },
+  }
 }
+
